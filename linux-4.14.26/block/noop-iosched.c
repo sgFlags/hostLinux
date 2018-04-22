5ecbc9c3fd82558c0da7f243e44647e41eaea512
sgFlags <wb2331@columbia.edu>
2018-04-21 22:21:37 -0400

    debug

--- linux-4.14.26/block/noop-iosched.c
+++ linux-4.14.26/block/noop-iosched.c
@@ -75,7 +75,7 @@ static int noop_dispatch(struct request_queue *q, int force)
     for (node = rb_first(&vmd->procs_vt_root); node != rb_last(&vmd->procs_vt_root); node = rb_next(node)) {
         procd = rb_entry(node, struct proc_data, proc_vt_node);
         if (!list_empty(&procd->request_list)) {
-            //printk(KERN_ERR "strange!!\n");
+            printk(KERN_ERR "strange!!\n");
             //goto my_fail;
             rq = list_last_entry(&procd->request_list, struct request, tag_list);
             break;
@@ -83,14 +83,14 @@ static int noop_dispatch(struct request_queue *q, int force)
     }
     if (!rq) {
         if (!node) {
-            //printk(KERN_ERR "strange!!\n");
+            printk(KERN_ERR "strange 1!!\n");
             goto my_fail;
         } else {
             procd = rb_entry(node, struct proc_data, proc_vt_node); 
             if (!list_empty(&procd->request_list)) {
                 rq = list_last_entry(&procd->request_list, struct request, tag_list);
             } else {
-                //printk(KERN_ERR "strange!!\n");
+                printk(KERN_ERR "strange 2!!\n");
                 goto my_fail;
             }
         }
