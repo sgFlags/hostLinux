5bb851fed80238445b7bb6b8f56596c9fdaf603f
sgFlags <wb2331@columbia.edu>
2018-04-21 23:14:30 -0400

    debug

--- linux-4.14.26/block/noop-iosched.c
+++ linux-4.14.26/block/noop-iosched.c
@@ -168,7 +168,7 @@ my_fail:
     list_for_each_entry(temp_rq, &nd->queue, queuelist) {
         if (temp_rq->tagio.tag_flags != FLAG_TAG) {
             req = temp_rq;
-            if (temp->tagio.tag_flags == tag_ok)
+            if (req->tagio.tag_flags == tag_ok)
                 list_del_init(&req->tag_list);
             break;
         }
